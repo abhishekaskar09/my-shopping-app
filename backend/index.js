20090d921aa99e280d53158b7bb29454ea606520
Abhishek Askar <abhishekasker084@gmail.com>
2026-05-18 15:27:19 +0530

    debug vercel mongo

--- backend/index.js
+++ backend/index.js
@@ -46,4 +46,13 @@ app.use((err, req, res, next) => {
     });
 });
 
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch((err) => {
+        console.log("❌ DB Connection Error:", err);
+    });
+
 module.exports = app;
