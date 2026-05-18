767ac542042cc25323abac11f7b657c6fe0d63bc
Abhishek Askar <abhishekasker084@gmail.com>
2026-05-18 16:04:13 +0530

    fix vercel backend

--- backend/index.js
+++ backend/index.js
@@ -6,53 +6,23 @@ const cors = require("cors");
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
 const ProductRoutes = require("./routes/ProductRoutes");
 const AuthRoutes = require("./routes/AuthRoutes");
 const CartRoutes = require("./routes/CartRoutes");
 const OrderRoutes = require("./routes/OrderRoutes");
 const paymentRoutes = require("./routes/PaymentRoutes");
 
-// DB Connection
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ MongoDB connected"))
-    .catch((err) => console.log("❌ DB Connection Error:", err));
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ DB Connection Error:", err));
 
-// APIs
 app.use("/api/products", ProductRoutes);
 app.use("/api/auth", AuthRoutes);
 app.use("/api/carts", CartRoutes);
 app.use("/api/orders", OrderRoutes);
 app.use("/api/payment", paymentRoutes);
-
-// 404 Middleware
-app.use((req, res, next) => {
-    const error = new Error(`direction not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
-
-// Error Middleware
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-
-    res.status(statusCode).json({
-        message: err.message,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
-});
-
-
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ MongoDB connected"))
-    .catch((err) => {
-        console.log("❌ DB Connection Error:", err);
-    });
 
 module.exports = app;
